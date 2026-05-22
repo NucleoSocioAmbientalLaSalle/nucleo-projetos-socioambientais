945ee3a84df3c483888722c1393ad56acf85dea7
VictorCardosoPDev <vic.koopa45@gmail.com>
2026-05-22 18:10:16 -0300

    feature - adição de animação no header

--- scripts/index.js
+++ scripts/index.js
@@ -117,3 +117,15 @@ collapsibleCards.forEach(card => {
         card.classList.toggle("active");
     });
 });
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+
+});
